29e13ffdf8ed14c4b5215e22f5cd681b5d45a179
James McCullough <McCullough-J28@ulster.ac.uk>
2025-11-07 15:44:36 +0000

    changes made

--- Assets/_Game/Code/bat_rotation.cs
+++ Assets/_Game/Code/bat_rotation.cs
@@ -6,13 +6,25 @@ using UnityEngine.Rendering;
 
 public class bat_rotation : MonoBehaviour
 {
+    [SerializeField] private Camera main_camera;
+    private UnityEngine.Vector2 mouse_position;
 
     // Update is called once per frame
     void Update()
     {
-        transform.LookAt(Input.mousePosition);
+        //transform.LookAt(Input.mousePosition);
 
+        mouse_position = Input.mousePosition;
+
+        UnityEngine.Vector3 world_position = main_camera.ScreenToWorldPoint(new UnityEngine.Vector3(mouse_position.x, mouse_position.y, main_camera.nearClipPlane));
+
+        UnityEngine.Vector3 rotation_direction = (world_position - transform.position).normalized;
+        rotation_direction.z = 0;
+
+        float angle = Mathf.Atan2(rotation_direction.y, rotation_direction.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(new UnityEngine.Vector3(0, 0, angle));
 
 
     }
+    
 }
