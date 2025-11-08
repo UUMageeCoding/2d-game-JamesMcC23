ea077648daa6d3ede67011db307da11253345c23
JamesMcC23 <McCullough-J28@ulster.ac.uk>
2025-11-08 18:49:15 +0000

    IT ACTUALLY WORKS!?!?!

--- Assets/_Game/Code/bat_rotation.cs
+++ Assets/_Game/Code/bat_rotation.cs
@@ -16,7 +16,7 @@ public class bat_rotation : MonoBehaviour
 
         mouse_position = Input.mousePosition;
 
-        UnityEngine.Vector3 world_position = main_camera.ScreenToWorldPoint(new UnityEngine.Vector3(mouse_position.x, mouse_position.y, main_camera.nearClipPlane));
+        UnityEngine.Vector3 world_position = main_camera.ScreenToWorldPoint(new UnityEngine.Vector3(mouse_position.x, mouse_position.y, main_camera.nearClipPlane + 30));
 
         UnityEngine.Vector3 rotation_direction = (world_position - transform.position).normalized;
         rotation_direction.z = 0;
